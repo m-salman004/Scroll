3967611c3cf2676c559c73123bc4dd9812183706
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-02 18:10:19 -0800

    Close Links

--- app.js
+++ app.js
@@ -7,5 +7,21 @@
 // select span
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+  // console.log(linksContainer.getBoundingClientRect());
+});
 
