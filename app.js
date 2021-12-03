2c1cb04095a075f7b1e0747d94927ebb62bcbf0a
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-02 18:11:19 -0800

    fixed navbar

--- app.js
+++ app.js
@@ -24,4 +24,16 @@ navToggle.addEventListener("click", function () {
   }
   // console.log(linksContainer.getBoundingClientRect());
 });
+// ********** fixed navbar ************
 
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
